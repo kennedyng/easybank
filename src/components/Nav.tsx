459e1c1a9153f834fca46f6b749347c91f708009
kennedyng <68716247+kennedyng@users.noreply.github.com>
2023-04-20 00:05:08 +0200

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const links = [
   {
@@ -26,18 +26,41 @@ const links = [
 ];
 
 const Nav = () => {
+  const [open, setOpenMenu] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setOpenMenu(!open);
+  };
   return (
-    <nav className="container mx-auto flex flex-row items-center justify-between py-4">
+    <nav className="container mx-auto flex flex-row items-center justify-between p-4">
       <img src="/images/logo.svg" alt="easybank" />
-      <ul className="flex flex-row space-x-4 text-slate-400">
+      <ul className="flex-row space-x-4 text-slate-400 hidden  md:flex">
         {links.map(({ label, link }) => (
           <li key={label}>{label}</li>
         ))}
       </ul>
 
-      <div className="bg-gradient-to-r  from-green-400 to-blue-400  px-4 py-2 rounded-3xl text-white">
+      <div className="bg-gradient-to-r  from-green-400 to-blue-400  px-4 py-2 rounded-3xl text-white hidden md:block">
         Request Invite
       </div>
+
+      <div onClick={toggleMenu} className="md:hidden sm:block">
+        {!open ? (
+          <img src="/images/icon-hamburger.svg" alt="" />
+        ) : (
+          <img src="/images/icon-close.svg" alt="" />
+        )}
+      </div>
+
+      {open && (
+        <div className="absolute bg-slate-400 w-11/12 top-20 p-4 rounded-md">
+          <ul className="flex flex-col justify-center items-center space-y-4">
+            {links.map(({ label, link }) => (
+              <li key={label}>{label}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
